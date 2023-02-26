c819ffbe74bdfba07ad6c20200f3ffa11baab015
Nixon <voxell.technologies@gmail.com>
2023-02-26 16:31:45 +0800

    setup pico vr headset

---
+++ Nixon/Hilti2023_DeltaAPU_Unity/Assets/Scripts/Chatbot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Chatbot : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
