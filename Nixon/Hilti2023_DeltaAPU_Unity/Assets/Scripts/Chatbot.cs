a5170cf002abb108cd9036210171376ed66d6bc6
Nixon <voxell.technologies@gmail.com>
2023-02-26 17:53:40 +0800

    add uxml asset reference to Chatbot

--- Nixon/Hilti2023_DeltaAPU_Unity/Assets/Scripts/Chatbot.cs
+++ Nixon/Hilti2023_DeltaAPU_Unity/Assets/Scripts/Chatbot.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UIElements;
 
 public class Chatbot : MonoBehaviour
 {
-    private void Start()
+    [SerializeField]
+    private VisualTreeAsset m_UxmlAsset = default;
+
+    private VisualElement m_Root;
+    private TextInputBaseField<string> m_Chatbox;
+
+    private void Awake()
     {
-        
+        this.m_Root = this.m_UxmlAsset.Instantiate();
+        this.m_Chatbox = this.m_Root.Q<TextInputBaseField<string>>("chatbox");
+        this.m_Chatbox.value = "test";
     }
 
     private void Update()
