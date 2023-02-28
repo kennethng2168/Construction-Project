e6fd368650eb0ca5d2b52c74231b650da53fbfde
Nixon <voxell.technologies@gmail.com>
2023-02-28 16:47:17 +0800

    remove table and enlarge map

--- Nixon/Hilti2023_DeltaAPU_Unity/Assets/Scripts/MapController.cs
+++ Nixon/Hilti2023_DeltaAPU_Unity/Assets/Scripts/MapController.cs
@@ -5,30 +5,45 @@ public class MapController : XRGrabInteractable
 {
     [SerializeField] private Rigidbody m_RigidBody;
     [SerializeField] private Transform m_Transform;
+    [SerializeField] private float m_Damping = 0.9f;
     private Vector3 m_OriginLocalTrans;
     private Quaternion m_OriginLocalRot;
+
+    private Vector3 m_PrevLocalTrans;
+    private Vector3 m_Velocity;
 
     private void Start()
     {
         this.m_OriginLocalTrans = this.m_Transform.localPosition;
         this.m_OriginLocalRot = this.m_Transform.localRotation;
+
+        this.m_PrevLocalTrans = this.m_OriginLocalTrans;
+        this.m_Velocity = Vector3.zero;
     }
 
-    private void LateUpdate()
+    public void Update()
     {
-        // this.m_Transform.localRotation = this.m_OriginLocalRot;
-        // Vector3 currLocalTrans = this.m_Transform.localPosition;
-        // currLocalTrans.y = this.m_OriginLocalTrans.y;
-        // this.m_Transform.localPosition = currLocalTrans;
+        // remove y axis velocity
+        this.m_Velocity.y = 0.0f;
+        this.m_Transform.localPosition += this.m_Velocity * Time.deltaTime;
+
+        this.m_Velocity = (this.m_Transform.localPosition - this.m_PrevLocalTrans) / Time.deltaTime;
+        this.m_PrevLocalTrans = this.m_Transform.localPosition;
+        this.m_Velocity *= this.m_Damping;
     }
 
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
     {
+        if (Time.deltaTime == 0.0f) return;
+
         base.ProcessInteractable(updatePhase);
 
-        this.m_Transform.localRotation = this.m_OriginLocalRot;
-        Vector3 currLocalTrans = this.m_Transform.localPosition;
-        currLocalTrans.y = this.m_OriginLocalTrans.y;
-        this.m_Transform.localPosition = currLocalTrans;
+        if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.OnBeforeRender)
+        {
+            this.m_Transform.localRotation = this.m_OriginLocalRot;
+            Vector3 currLocalTrans = this.m_Transform.localPosition;
+            currLocalTrans.y = this.m_OriginLocalTrans.y;
+            this.m_Transform.localPosition = currLocalTrans;
+        }
     }
 }
